7188651b1bf3cffd1a140b7a6e5dd028546c0590
udawe78 <udawe78@gmail.com>
2023-10-13 18:05:45 +0000

    remove_non_alphanumeric() added

--- ContentAutomator/Postify/LinkedIn/src/apiPosting.py
+++ ContentAutomator/Postify/LinkedIn/src/apiPosting.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import random
 import sys
 from datetime import datetime
+import re
 
 from logger import logger_setup
 from env import ACCESS_TOKEN, ID_COMPANY, TARGET_URL, DEFAULT_POST_FOLDER, FILES_FOLDER, CONSTANT_HASHTAGS
@@ -11,22 +12,31 @@ from env import ACCESS_TOKEN, ID_COMPANY, TARGET_URL, DEFAULT_POST_FOLDER, FILES
 
 timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 logger = logger_setup(f'{Path(__file__).stem}')
-# logger = logger_setup(f'{Path(__file__).stem}_{timestamp}')
+
+
+def remove_non_alphanumeric(raw: str) -> str:
+    invalid_symbols = r'[^a-zA-Z0-9äöüÄÖÜßàáâãäåæçèéêëìíîïðòóôõöøùúûüýÿ]'
+    return re.sub(invalid_symbols, '', raw)
 
 
 def get_request_data(request_data_path: str) -> tuple:
     try:
         with open(Path(request_data_path), 'r') as f:
             request_data = json.load(f)
+            
         return request_data['api_url'], request_data['headers'], request_data['request_body']
+    
+    except FileNotFoundError as err:
+        file_path = Path(err.filename)
+        logger.critical(f'Input file: "../{file_path.parent.name}/{file_path.name}" is not found')
+        
     except Exception as err:
-        print(f'{type(err).__name__:} {err}')
+        logger.error(f'{type(err).__name__}: {err}')
         
 
-def get_post_to_share(post_folder: str) -> Path:
+def choice_post_to_share(post_folder: str) -> Path:
     try:
         post_folder = Path(post_folder)
-        
         posted_path = Path(f'{FILES_FOLDER}/linkedin_posted.json')
         with open(posted_path, 'r') as f:
             posted = json.load(f)
@@ -34,10 +44,19 @@ def get_post_to_share(post_folder: str) -> Path:
         json_ids = set(v.stem for v in post_folder.glob('*.json'))
         random_json_id = random.choice(list(json_ids.difference(posted['posted'])))
         to_post_path = next(post_folder.glob(f'{random_json_id}.json'))
-        logger.info(f'Post to share: {to_post_path.name}')
         
         return to_post_path
     
+    except FileNotFoundError as err:
+        if err.filename == str(posted_path):
+            with open(posted_path, 'w') as f:
+                json.dump({'posted':[]}, f, indent=4)
+            logger.warning(f'File "{posted_path.parent.name}/{posted_path.name}" was created from scratch')
+        
+            return choice_post_to_share(post_folder)
+        
+        logger.error(f'{type(err).__name__}: {err.filename}')   
+                
     except Exception as err:
         logger.error(f'{type(err).__name__}: {err}')
 
@@ -46,25 +65,22 @@ def get_post_content(file_path: Path) -> dict:
     try:
         with open(file_path, 'r') as f:
             post_content=json.load(f)
-            logger.info(f'Post content extracted from: {file_path.name}')
+            
         return post_content
+    
     except Exception as err:
-        print(f'{type(err).__name__:} {err}')
+        logger.error(f'{type(err).__name__}: {err}')
     
     
-def prepare_text_to_share(data: dict) -> str:
+def prepare_text_to_share(name: str, location: str, title: str, text: str, hashtags: list) -> str:
     try:
-        hashtags_top = f'#{"".join(data["name"].split())} '\
-                    f'#{"".join(data["location"].split(", ")[0])} '\
-                    f'#{"".join(data["location"].split(", ")[1])}'    
-        
-        title = data['title']
-        text = data['text']
-        hashtags_bottom = CONSTANT_HASHTAGS + ' ' + ' '.join(data['hashtags'])
+        city, country = location.split(', ')
+        name, city, country = map(remove_non_alphanumeric, (name, city, country))
+                
+        hashtags_top = f'#{name} #{city} #{country}'    
+        hashtags_bottom = CONSTANT_HASHTAGS + ' ' + ' '.join(hashtags)
     
         text_to_share = f'{hashtags_top}\n\n{title}\n\n{text}\n\nFind out more at {TARGET_URL}\n\n{hashtags_bottom}\n'
-        
-        logger.info(f'Text to share is prepared: {data["name"]} in {data["location"]}')
         
         return text_to_share
     
@@ -75,22 +91,17 @@ def prepare_text_to_share(data: dict) -> str:
 def register_image() -> tuple():
     try:
         api_url, headers, request_body = get_request_data(f'{FILES_FOLDER}/schema_request_register_image.json')
-        logger.info(f'Request schema is parsed')
         
-        # insert credentials
+        # insert credentials into request body
         headers['Authorization'] = headers['Authorization'].format(access_token = ACCESS_TOKEN)
         request_body['registerUploadRequest']['owner'] = request_body['registerUploadRequest']['owner'].format(id_company = ID_COMPANY)
-        logger.info(f'Credentials are added into the request body')
         
-        logger.info(f'POST request is started')
         response = requests.post(api_url, json=request_body, headers=headers)
         response.raise_for_status()
-        logger.info(f'Response received with a status code: {response.status_code}')
        
         data = json.loads(response.text)
         upload_url = data['value']['uploadMechanism']['com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest']['uploadUrl']
         asset = data['value']['asset']
-        logger.info(f'Upload Url and asset are parsed')
         
         return upload_url, asset
     
@@ -105,93 +116,85 @@ def upload_binary_image(image_path: str, upload_url: str) -> None:
         image_path = Path(image_path.replace(TARGET_URL, '/home/azureuser'))
         with open(image_path, 'rb') as f:
             binary_image = f.read()
-        logger.info(f'The binary mode image is obtained: {image_path.name}')
-        
+                
         headers = {
             'Authorization': f'Bearer {ACCESS_TOKEN}', 
             'X-Restli-Protocol-Version': '2.0.0'
         }
 
-        logger.info(f'POST request is started')
         response = requests.post(upload_url, binary_image, headers=headers)
         response.raise_for_status()
-        logger.info(f'Response received with a status code: {response.status_code}')
     
     except Exception as err:
         logger.error(f'{type(err).__name__}: {err}')
     
     
-def share_text_and_image(text_to_share: str, asset: str) -> None:
+def share_text_and_image(text_to_share: str, asset: str) -> str:
     try:
         api_url, headers, request_body = get_request_data(f'{FILES_FOLDER}/schema_request_text_image_share.json')
-        logger.info(f'Request schema is parsed')
-        
-        # insert credentials
+                
+        # insert credentials into request body
         headers['Authorization'] = headers['Authorization'].format(access_token = ACCESS_TOKEN)
         request_body['author'] = request_body['author'].format(id_company = ID_COMPANY)
-        logger.info(f'Credentials are added into the request body')
         
         # assign input values
         request_body['specificContent']['com.linkedin.ugc.ShareContent']['shareCommentary']['text'] = text_to_share
         request_body['specificContent']['com.linkedin.ugc.ShareContent']['media'][0]['media'] = asset
-        logger.info(f'Text to share and asset are added into the request body')
 
-        logger.info(f'POST request is started')
         response = requests.post(api_url, headers=headers, json=request_body)
         response.raise_for_status()
+        
         if response.status_code == 201:
-            logger.info('Post successfully created!')
-            # print('Post successfully created!')
+            return 'Post from {from_} about {about_} in {in_} is shared SUCCESFULLY'
             
     except Exception as err:
         logger.error(f'{type(err).__name__}: {err}') 
 
 
-def add_to_posted(file_path: Path) -> None:
+def add_to_posted(post_id: str) -> None:
     try:
         posted_path = Path(f'{FILES_FOLDER}/linkedin_posted.json')
-        
         with open(posted_path, 'r') as f:
             posted = json.load(f)
             
-        posted['posted'].append(file_path.stem)
+        posted['posted'].append(post_id)
         
         with open(posted_path, 'w') as f:
             json.dump(posted, f, indent=4)
-
-        logger.info(f'File {file_path.name} is added to posted files list')
+            
+    except FileNotFoundError as err:
+        file_path = Path(err.filename)
+        logger.critical(f'Input file: "../{file_path.parent.name}/{file_path.name}" not found')
         
     except Exception as err:
         logger.error(f'{type(err).__name__}: {err}') 
 
 
 def main(lang: str='en'):
-    # choice *.json from the posts folder en/ by default
+    # choice *.json from the posts folder en/ (by default)
     post_folder = f'{DEFAULT_POST_FOLDER}/{lang}'
-    logger.info(f'Posting process is started with source: {post_folder}')
-    post_to_share = get_post_to_share(post_folder)
+    post_to_share = choice_post_to_share(post_folder)
     
     # get content of choiced json
     post_content = get_post_content(post_to_share)
     
     # from json content compile name, location, title, text, hashtags into the one object
     text_data = {k: v for k, v in post_content.items() if k not in ('links', 'images')}
-    text_to_share = prepare_text_to_share(text_data)
+    text_to_share = prepare_text_to_share(**text_data)
     
-    # get image url and make requests to register image and ...
+    # request to register image and ...
     upload_url, asset = register_image()
     
-    # ... then upload binary image file
+    # ... then upload binary image file ...in order to ...
     image_path = post_content['images'][0]
     upload_binary_image(image_path, upload_url)
     
-    # share the text and the image
-    share_text_and_image(text_to_share, asset)
+    # ... share both the text and the image
+    result = share_text_and_image(text_to_share, asset)
+    logger.info(result.format(from_=post_to_share.name, about_=text_data['name'], in_=text_data['location']))
     
     # add file name in posted file
-    add_to_posted(post_to_share)
-    
-    logger.info(f'Posting process is finished successfully!')
+    add_to_posted(post_to_share.stem)
 
 
 if __name__ == '__main__':
@@ -202,4 +205,3 @@ if __name__ == '__main__':
         main(sys.argv[1])
     else:
         main()
-  
