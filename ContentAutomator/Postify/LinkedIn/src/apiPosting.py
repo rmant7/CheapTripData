76a751688c47d0a997baed9ced6404621e283aa3
udawe78 <udawe78@gmail.com>
2023-10-15 18:41:43 +0000

    renaming more contextually

--- ContentAutomator/Postify/LinkedIn/src/apiPosting.py
+++ ContentAutomator/Postify/LinkedIn/src/apiPosting.py
@@ -14,6 +14,7 @@ from env import ACCESS_TOKEN, ID_COMPANY, TARGET_URL, DEFAULT_POST_FOLDER, FILES
 timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 logger = logger_setup(f'{Path(__file__).stem}')
 
+# custom Exceptions to apart them from the exceptions raised in main()
 class FuncException(Exception):
     pass
 
@@ -44,7 +45,7 @@ def get_request_data(request_data_path: str) -> tuple:
         raise FuncException from err
         
 
-def get_post_to_share(post_folder: str) -> Path:
+def get_json_path(post_folder: str) -> Path:
     try:
         post_folder = Path(post_folder)
         posted_path = Path(f'{FILES_FOLDER}/linkedin_posted.json')
@@ -63,7 +64,7 @@ def get_post_to_share(post_folder: str) -> Path:
                 json.dump({'posted':[]}, f, indent=4)
             logger.warning(f'File "{posted_path.parent.name}/{posted_path.name}" was created from scratch')
         
-            return get_post_to_share(post_folder)
+            return get_json_path(post_folder)
         
         logger.error(f'{type(err).__name__}: {err.filename}')
         raise FuncException from err
@@ -73,7 +74,7 @@ def get_post_to_share(post_folder: str) -> Path:
         raise FuncException from err
         
 
-def get_post_content(file_path: Path) -> dict:
+def get_content(file_path: Path) -> dict:
     try:
         with open(file_path, 'r') as f:
             post_content=json.load(f)
@@ -91,7 +92,7 @@ def get_post_content(file_path: Path) -> dict:
         raise FuncException from err
     
     
-def prepare_text_to_share(name: str, location: str, title: str, text: str, hashtags: list, **_) -> str:
+def prepare_text_to_post(name: str, location: str, title: str, text: str, hashtags: list, **_) -> str:
     try:
         const_hashtags = {'#CheapTripGuru', '#travel', '#cheaptrip', '#budgettravel', '#travelonabudget', '#lowcosttravel',
                             '#affordabletravel', '#backpackerlife', '#cheapholidays', '#travelbudgeting', '#frugaltravel', 
@@ -135,7 +136,7 @@ async def register_image() -> tuple():
         raise FuncException from err
     
     
-async def get_binary_image(images: list, **_):
+async def get_binary_image(images: list, **_) -> bytes:
     try:
         image_path = Path(images[0].replace(TARGET_URL, '/home/azureuser'))
         with open(image_path, 'rb') as f:
@@ -161,7 +162,7 @@ def upload_binary_image(upload_url: str, binary_image: bytes) -> None:
         raise FuncException from err
     
     
-def share_text_and_image(text_to_share: str, asset: str) -> str:
+def post_text_and_image(text_to_share: str, asset: str) -> str:
     try:
         api_url, headers, request_body = get_request_data(f'{FILES_FOLDER}/schema_request_text_image_share.json')
                 
@@ -177,7 +178,7 @@ def share_text_and_image(text_to_share: str, asset: str) -> str:
         response.raise_for_status()
         
         if response.status_code == 201:
-            return 'Post from {from_} about {about_} in {in_} is shared SUCCESFULLY'
+            return 'Post from {} about {} in {} is shared SUCCESFULLY'
             
     except Exception as err:
         logger.error(f'{type(err).__name__}: {err}') 
@@ -207,28 +208,28 @@ def add_to_posted(post_id: str) -> None:
 
 async def main(lang: str='en'):
     try:
-        # choice *.json from the posts folder en/ (by default)
-        post_to_share = get_post_to_share(f'{DEFAULT_POST_FOLDER}/{lang}')
+        # get random *.json from the jsons folder en/ (by default)
+        json_path = get_json_path(f'{DEFAULT_POST_FOLDER}/{lang}')
         
         # get content of choiced json
-        post_content = get_post_content(post_to_share)
+        content = get_content(json_path)
         
         # joining hashtags, removing the last line break, etc.
-        text_to_share = prepare_text_to_share(**post_content)
+        text_to_post = prepare_text_to_post(**content)
         
         # get binary image from path and register image concurrently
-        binary_image, (upload_url, asset) = await gather(get_binary_image(**post_content), register_image()) 
+        binary_image, (upload_url, asset) = await gather(get_binary_image(**content), register_image()) 
        
         # upload binary image file
         upload_binary_image(upload_url, binary_image)
             
         # share both the text and the image
-        result = share_text_and_image(text_to_share, asset)
+        result = post_text_and_image(text_to_post, asset)
             
         # add file name in posted file
-        add_to_posted(post_to_share.stem)
+        add_to_posted(json_path.stem)
         
-        logger.info(result.format(from_=post_to_share.name, about_=post_content['names'], in_=post_content['location']))
+        logger.info(result.format(json_path.name, content['name'], content['location']))
         
     except FuncException:
         pass
